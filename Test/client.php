1c9f4a4b8855555d159118a98ee2166367648dfb
Séverine Vos <Sverine Vos>
2017-07-08 18:00:24 +0200

    client

--- Test/client.php
+++ Test/client.php
@@ -28,7 +28,7 @@ $client->get("/user", function(Response $response){
 $data = ["email" => "postme@localhost"];
 
 $client->post("/user", $data, function(Response $response){
-	echo "<h4>POST /user</h4>";
+	echo "<h4>POST /api/v1/user</h4>";
 	echo "<div>code: " . $response->getCode() . "</div>";
 	foreach ($response->getHeaders() as $name => $value) {
 		echo "<div>{$name}: {$value}</div>";
@@ -42,7 +42,7 @@ $client->post("/user", $data, function(Response $response){
 $data = ["email" => "putme@localhost"];
 
 $client->put("/user/1", $data, function(Response $response){
-	echo "<h4>PUT /user/1</h4>";
+	echo "<h4>PUT /api/v1/user/1</h4>";
 	echo "<div>code: " . $response->getCode() . "</div>";
 	foreach ($response->getHeaders() as $name => $value) {
 		echo "<div>{$name}: {$value}</div>";
@@ -56,7 +56,7 @@ $client->put("/user/1", $data, function(Response $response){
 $data = ["email" => "patchme@localhost"];
 
 $client->patch("/user/1", $data, function(Response $response){
-	echo "<h4>PATCH /user/1</h4>";
+	echo "<h4>PATCH /api/v1/user/1</h4>";
 	echo "<div>code: " . $response->getCode() . "</div>";
 	foreach ($response->getHeaders() as $name => $value) {
 		echo "<div>{$name}: {$value}</div>";
@@ -69,7 +69,7 @@ $client->patch("/user/1", $data, function(Response $response){
  */
 
 $client->delete("/user/1", function(Response $response){
-	echo "<h4>DELETE /user</h4>";
+	echo "<h4>DELETE /api/v1/user</h4>";
 	echo "<div>code: " . $response->getCode() . "</div>";
 	foreach ($response->getHeaders() as $name => $value) {
 		echo "<div>{$name}: {$value}</div>";
